0fa75cc6b811359339f48f7394eec1ca76a056f2
theendercore <theendercore@gmail.com>
2024-10-29 19:34:34 +0200

    - candy re-balancing

--- src/main/kotlin/org/teamvoided/voided_delight/item/VDFoodComponents.kt
+++ src/main/kotlin/org/teamvoided/voided_delight/item/VDFoodComponents.kt
@@ -4,15 +4,15 @@ import net.minecraft.entity.player.HungerConstants
 import net.minecraft.item.FoodComponent
 import java.util.*
 
-@Suppress("HasPlatformType", "MagicNumber")
+@Suppress("MagicNumber")
 object VDFoodComponents {
     val CANDY_BERRY = foodComponent(2, 0.2f, 0.8f)
     val MARSHMAROW = foodComponent(2, 0.2f, 0.8f)
-    val LOLLIPOP = foodComponent(2, 0.2f, 1.6f)
+    val LOLLIPOP = foodComponent(16, 0.4f, 1.6f)
     val CANDY_CORN = foodComponent(2, 0.2f, 0.8f)
     val LITCHORRICE = foodComponent(2, 0.2f, 0.8f)
-    val CANDY_CLOUD = foodComponent(2, 0.2f, 0.8f)
-    val SYRUP_APPLE = foodComponent(26, 0.4f, 1.2f)
+    val CANDY_CLOUD = foodComponent(2, 0.2f, 0.6f)
+    val SYRUP_APPLE = foodComponent(12, 0.3f, 1.2f)
 
     val CRYSTAL_CANDY_8 = foodComponent(8, 0.8f, 3f) //full block
     val CRYSTAL_CANDY_6 = foodComponent(6, 0.6f, 2f) //stair and wall block
@@ -21,7 +21,7 @@ object VDFoodComponents {
 
     fun foodComponent(hunger: Int, saturation: Float, eatSeconds: Float): FoodComponent {
         val saturationButComponent: Float = HungerConstants.calculateSaturation(hunger, saturation)
-        return FoodComponent(hunger, saturationButComponent, false, eatSeconds, Optional.empty(), listOf());
+        return FoodComponent(hunger, saturationButComponent, false, eatSeconds, Optional.empty(), listOf())
     }
     //1.6
 }
