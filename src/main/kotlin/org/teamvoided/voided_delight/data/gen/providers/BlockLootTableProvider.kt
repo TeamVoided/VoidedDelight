aa7ca4a9a0e26d8d2a8739a2f43be9d7b8282801
TheEnderCore <theendercore@gmail.com>
2026-03-06 20:22:36 +0200

    datagen conditions (unused for now)

--- src/main/kotlin/org/teamvoided/voided_delight/data/gen/providers/BlockLootTableProvider.kt
+++ src/main/kotlin/org/teamvoided/voided_delight/data/gen/providers/BlockLootTableProvider.kt
@@ -2,9 +2,11 @@ package org.teamvoided.voided_delight.data.gen.providers
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions
 import net.minecraft.block.Block
 import net.minecraft.block.DoorBlock
 import net.minecraft.block.SlabBlock
+import net.minecraft.data.server.loot_table.BlockLootTableGenerator
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTable
 import net.minecraft.loot.condition.BlockStatePropertyLootCondition
@@ -12,6 +14,7 @@ import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider
 import net.minecraft.predicate.StatePredicate
 import net.minecraft.registry.HolderLookup
+import org.teamvoided.voided_delight.VDCompat
 import org.teamvoided.voided_delight.init.VDBlocks
 import vectorwing.farmersdelight.common.block.FeastBlock
 import vectorwing.farmersdelight.common.block.PieBlock
@@ -21,29 +24,38 @@ class BlockLootTableProvider(o: FabricDataOutput, r: CompletableFuture<HolderLoo
     FabricBlockLootTableProvider(o, r) {
     val manualList: List<Block> = listOf()
 
+    val dndGen = conditional(VDCompat.DND)
+    val whitePumpkinsGen = conditional(VDCompat.DND)
+
     override fun generate() {
         for (block in VDBlocks.BLOCKS.filterNot(manualList::contains)) {
-            when (block) {
-                is SlabBlock -> add(block, ::slabDrops)
-                is DoorBlock -> add(block, ::doorDrops)
-                is FeastBlock -> add(block, ::feastDrops)
-                is PieBlock -> add(block) { dropsNothing() }
-                else -> addDrop(block)
-            }
+            processBlock(block)
         }
     }
 
-    fun feastDrops(it: Block): LootTable.Builder {
-        return LootTable.builder().pool(
-            applySurvivesExplosionCondition(
-                it, LootPool.builder()
-                    .rolls(ConstantLootNumberProvider.create(1.0f))
-                    .conditionally(
-                        BlockStatePropertyLootCondition.builder(it)
-                            .properties(StatePredicate.Builder.create().exactMatch(FeastBlock.SERVINGS, 4))
-                    )
-                    .with(ItemEntry.builder(it))
-            )
+    fun conditional(vararg id: String): BlockLootTableGenerator = withConditions(ResourceConditions.anyModsLoaded(*id))
+}
+
+fun BlockLootTableGenerator.feastDrops(it: Block): LootTable.Builder {
+    return LootTable.builder().pool(
+        applySurvivesExplosionCondition(
+            it, LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1.0f))
+                .conditionally(
+                    BlockStatePropertyLootCondition.builder(it)
+                        .properties(StatePredicate.Builder.create().exactMatch(FeastBlock.SERVINGS, 4))
+                )
+                .with(ItemEntry.builder(it))
         )
+    )
+}
+
+fun BlockLootTableGenerator.processBlock(block: Block) {
+    when (block) {
+        is SlabBlock -> add(block, ::slabDrops)
+        is DoorBlock -> add(block, ::doorDrops)
+        is FeastBlock -> add(block, ::feastDrops)
+        is PieBlock -> add(block) { BlockLootTableGenerator.dropsNothing() }
+        else -> addDrop(block)
     }
 }
