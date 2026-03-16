54e3b8f6e9f46950c929a737ded4d090f5c059b1
TheEnderCore <theendercore@gmail.com>
2026-03-16 21:03:44 +0200

    Skillets can now cook in ultrawarm dimensions

--- src/main/java/org/teamvoided/voided_delight/mixin/skillet/SkilletItemMixin.java
+++ src/main/java/org/teamvoided/voided_delight/mixin/skillet/SkilletItemMixin.java
@@ -1,9 +1,13 @@
 package org.teamvoided.voided_delight.mixin.skillet;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vectorwing.farmersdelight.common.item.SkilletItem;
 
 import static org.teamvoided.voided_delight.util.MixinHelpersKt.getSkilletCookingTimeModifier;
@@ -14,5 +18,12 @@ public class SkilletItemMixin {
     @ModifyExpressionValue(method = "getUseTicks", at = @At(value = "INVOKE", target = "Lvectorwing/farmersdelight/common/block/SkilletBlock;getSkilletCookingTime(II)I"))
     int addCookingTimeModifier(int original) {
         return (int) (original * getSkilletCookingTimeModifier((Item) (Object) this));
+    }
+
+    @Inject(method = "isPlayerNearHeatSource", at = @At("HEAD"), cancellable = true)
+    private static void isInHotDim(PlayerEntity player, WorldView level, CallbackInfoReturnable<Boolean> cir) {
+        if (level.getDimension().ultraWarm()) {
+            cir.setReturnValue(true);
+        }
     }
 }
